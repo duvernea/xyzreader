ada2d456b56f696d27bce4aea4e1452d61e973ff
Brian DuVerneay <duverneay@gmail.com>
2016-01-24 13:18:26 -0600

    Update OkHttp to 2.7 and change fetchPlainText method

--- XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
+++ XYZReader/src/main/java/com/example/xyzreader/remote/RemoteEndpointUtil.java
@@ -3,6 +3,8 @@ package com.example.xyzreader.remote;
 import android.util.Log;
 
 import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
 
 import org.json.JSONArray;
 import org.json.JSONException;
@@ -45,28 +47,32 @@ public class RemoteEndpointUtil {
     }
 
     static String fetchPlainText(URL url) throws IOException {
-        return new String(fetch(url), "UTF-8" );
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder().url(url).build();
+        Response response = client.newCall(request).execute();
+        return response.body().string();
     }
 
-    static byte[] fetch(URL url) throws IOException {
-        InputStream in = null;
-
-        try {
-            OkHttpClient client = new OkHttpClient();
-            HttpURLConnection conn = client.open(url);
-            ByteArrayOutputStream out = new ByteArrayOutputStream();
-            in = conn.getInputStream();
-            byte[] buffer = new byte[1024];
-            int bytesRead;
-            while ((bytesRead = in.read(buffer)) > 0) {
-                out.write(buffer, 0, bytesRead);
-            }
-            return out.toByteArray();
-
-        } finally {
-            if (in != null) {
-                in.close();
-            }
-        }
-    }
+//    static byte[] fetch(URL url) throws IOException {
+//        InputStream in = null;
+//
+//        try {
+//            OkHttpClient client = new OkHttpClient();
+//            HttpURLConnection conn = client.open(url);
+//            ByteArrayOutputStream out = new ByteArrayOutputStream();
+//            in = conn.getInputStream();
+//            byte[] buffer = new byte[1024];
+//            int bytesRead;
+//            while ((bytesRead = in.read(buffer)) > 0) {
+//                out.write(buffer, 0, bytesRead);
+//            }
+//            return out.toByteArray();
+//
+//        } finally {
+//            if (in != null) {
+//                in.close();
+//            }
+//        }
+//    }
 }
